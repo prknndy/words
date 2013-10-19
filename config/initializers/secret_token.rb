700a79d1cc70acc44e45b816a916300c07f3ff05
Peter Kennedy <prknndy@gmail.com>
2013-10-19 17:31:32 -0500

    Initial repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Words::Application.config.secret_key_base = 'e82afb9d8f5035a69ba13140d27543abf32c7fd693d6a2d63e5399fc2bb6007737974bcab8bfd19eca94d58dcd4cabd0fc2b4745f21f0fad331e017b83a3bb7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Words::Application.config.session_store :cookie_store, key: '_words_session'
